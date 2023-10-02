6403f3ce7f7a10ba18151eb7f1a5f534af8844a7
Juan Carlos Herrera Blandon <jcherrerab@indracompany.com>
2023-10-02 13:42:16 -0500

    Created method in author handler for find all authors

--- src/main/java/com/books/library/entitysBooks/controller/AuthorController.java
+++ src/main/java/com/books/library/entitysBooks/controller/AuthorController.java
@@ -2,25 +2,32 @@ package com.books.library.entitysBooks.controller;
 
 import com.books.library.entitysBooks.domain.dto.AuthorDTO;
 import com.books.library.entitysBooks.service.features.commands.CreateFabricAuthorService;
+import com.books.library.entitysBooks.service.features.queries.GetAllFabricAuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/library")
 public class AuthorController {
     private final CreateFabricAuthorService createFabricAuthorService;
+    private final GetAllFabricAuthorService getAllFabricAuthorService;
 
-    public AuthorController(CreateFabricAuthorService createFabricAuthorService) {
+    public AuthorController(CreateFabricAuthorService createFabricAuthorService, GetAllFabricAuthorService getAllFabricAuthorService) {
         this.createFabricAuthorService = createFabricAuthorService;
+        this.getAllFabricAuthorService = getAllFabricAuthorService;
     }
 
 
     @PostMapping("/author")
-    public AuthorDTO createAuthor(@RequestBody AuthorDTO authorDTO){
-        return createFabricAuthorService.save(authorDTO);
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO authorDTO){
+        return new ResponseEntity<>(createFabricAuthorService.save(authorDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping("/hellow")
-    public String getHellow(){
-        return "Hellow";
+    @GetMapping("/authors")
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors(){
+        return new ResponseEntity<>(getAllFabricAuthorService.findAll(),HttpStatus.OK);
     }
 }
